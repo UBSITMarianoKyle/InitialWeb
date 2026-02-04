4f60a9c45750b183e4452213fc7a4a2a21c23da2
x <x@f112.ub.lan>
2026-02-04 13:11:30 +0800

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterOutlet } from '@angular/router';
+import { NaviComponent } from './parts/navi/navi.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NaviComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
+import { ComponentRef } from '@angular/core';
 import { Routes } from '@angular/router';
+import { HomeComponent } from './parts/home/home.component';
+import { NaviComponent } from './parts/navi/navi.component';
+import { UserProfileComponent } from './AccctSettings/user-profile/user-profile.component';
+import { PaymentInfoComponent } from './AccctSettings/payment-info/payment-info.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'Home',
+    component:HomeComponent
+    },
+    {path:'Userbio',
+    component:UserProfileComponent}
+,
+    {path:'User Profile',
+    component:UserProfileComponent},
+   
+    {path:'Paymentinfo',
+    component:PaymentInfoComponent}
+
+
+
+
+
+
+
+
+
+
+
+];
